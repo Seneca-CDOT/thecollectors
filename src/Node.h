baf60ef8d4b9cde8ce7f89aef5e10ec91b7dea63
Andrei Kopytov <kopytov.andrei@gmail.com>
2013-05-29 17:10:46 -0400

    Created declaration file for the Node class

---
+++ src/Node.h
@@ -0,0 +1,4 @@
+// This file describes the function prototypes for the Node JavaScript class
+function Node(uID, vPosition)
+Node.prototype.position = function()
+Node.prototype.equals = function(node)
