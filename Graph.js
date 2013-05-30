777e1c81b903ac4bcedd7cfd735c49c3c039069e
Andrei Kopytov <kopytov.andrei@gmail.com>
2013-05-30 15:02:15 -0400

    Created declaration file for the Graph class and cleaned up and renamed NodeGraph.js to Graph.js to be more consistent with the declaration file

--- Graph.js
+++ Graph.js
@@ -1,19 +1,11 @@
-Structure = {
-    NONE : 0,
-    FUEL_STN : 1,
-    OFFICE : 2,
-    SCHOOL : 3
-}
-
-function nodeGraph() {
+function Graph() {
     this.nodeList = [];
-    this.nodeListLength = 0;
 
     // Dictionary of Node ID/index pairs
     this.nodeDictionary = {};
 }
 
-nodeGraph.prototype.addNode = function(node, connectionIdList) {
+Graph.prototype.addNode = function(node, connectionIdList) {
     if (this.nodeDictionary[node.id] != undefined) {
         console.warn("Node already exists in the graph. Duplicate attempt to add node terminated.");
         return;
@@ -24,8 +16,7 @@ nodeGraph.prototype.addNode = function(node, connectionIdList) {
     }
     if (node instanceof Node) {
         this.nodeList.push([node]);
-        this.nodeDictionary[node.id] = this.nodeListLength;
-        this.nodeListLength += 1;
+        this.nodeDictionary[node.id] = this.nodeList.length;
 
         var len = connectionIdList ? connectionIdList.length : 0;
         for (i = 0; i < len; i++) {
@@ -38,22 +29,13 @@ nodeGraph.prototype.addNode = function(node, connectionIdList) {
     }
 }
 
-nodeGraph.prototype.addEdge = function(edge) {
-    if (edge instanceof Edge) {
-        var nodeArray = this.findNodeArray(edge.vertexOneID);
-        nodeArray.push(edge);
-        nodeArray = this.findNodeArray(edge.vertexTwoID);
-        nodeArray.push(edge);
-    }
-}
-
-nodeGraph.prototype.clearGraph = function() {
+Graph.prototype.clearGraph = function() {
     this.nodeList = [];
     this.nodeListLength = 0;
     this.nodeDictionary = {};
 }
 
-nodeGraph.prototype.addConnections = function(nodeID, nodeToConnect) {
+Graph.prototype.addConnections = function(nodeID, nodeToConnect) {
     var nodeArray = this.findNodeArray(nodeID);
 
     if (nodeArray != undefined) {
@@ -63,11 +45,11 @@ nodeGraph.prototype.addConnections = function(nodeID, nodeToConnect) {
     }
 }
 
-nodeGraph.prototype.findNodeArray = function(nodeID) {
+Graph.prototype.findNodeArray = function(nodeID) {
     return this.nodeList[this.nodeDictionary[nodeID]];
 }
 
-nodeGraph.prototype.areNodesConnected = function(nodeID, nodeIDToMatch) {
+Graph.prototype.areNodesConnected = function(nodeID, nodeIDToMatch) {
     if (nodeID == nodeIDToMatch) {
         console.warn("Node cannot be connected to itself.");
         return false;
@@ -87,26 +69,5 @@ nodeGraph.prototype.areNodesConnected = function(nodeID, nodeIDToMatch) {
         return false;
     } else {
         console.error("Invalid NodeID. Node does not exist.");
-    }
-}
-
-nodeGraph.prototype.getEdgeConnections = function(nodeID) {
-    var nodeArray = this.findNodeArray(nodeID);
-
-    if (nodeArray != undefined) {
-        var edgeList = [];
-        var len = nodeArray.length;
-
-        for (i = 1; i < len; i++) {
-            var element = nodeArray[i];
-
-            if (element instanceof Edge) {
-                edgeList.push(element);
-            }
-        }
-
-        return edgeList;
-    } else {
-        console.error("Cannot find edge connections. Invalid nodeID.");
     }
 }
