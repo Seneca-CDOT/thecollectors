a75dff06dd9ee0c32c42e519b7019dc1d61f5e7f
dsegna <dsegna@myseneca.ca>
2013-06-05 14:05:35 -0400

    init

---
+++ MapGenerator.js
@@ -0,0 +1,11 @@
+function MapGenerator(difficulty){
+	this.mapGraph=new Graph();
+	this.structureList={};
+
+}
+MapGenerator.prototype.generateMapGraph = function() {
+	
+}
+MapGenerator.prototype.generateRoads = function(){
+
+}
